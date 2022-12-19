a3f1c391370c80599ca51b748a03a7025ba6e641
Andrija Sagicc <andrija.sagic@gmail.com>
2022-12-19 16:14:55 +0100

    Add large-v1 and large-v2 to CLIs
    Original Commit de77829e

--- cli.py
+++ cli.py
@@ -5,9 +5,8 @@ from urllib.parse import urlparse
 import warnings
 import numpy as np
 
-
 import torch
-from app import LANGUAGES, WhisperTranscriber
+from app import LANGUAGES, WHISPER_MODELS, WhisperTranscriber
 from src.download import download_url
 
 from src.utils import optional_float, optional_int, str2bool
@@ -16,7 +15,7 @@ from src.whisperContainer import WhisperContainer
 def cli():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("audio", nargs="+", type=str, help="audio file(s) to transcribe")
-    parser.add_argument("--model", default="small", choices=["tiny", "base", "small", "medium", "large"], help="name of the Whisper model to use")
+    parser.add_argument("--model", default="small", choices=WHISPER_MODELS, help="name of the Whisper model to use")
     parser.add_argument("--model_dir", type=str, default=None, help="the path to save model files; uses ~/.cache/whisper by default")
     parser.add_argument("--device", default="cuda" if torch.cuda.is_available() else "cpu", help="device to use for PyTorch inference")
     parser.add_argument("--output_dir", "-o", type=str, default=".", help="directory to save the outputs")
